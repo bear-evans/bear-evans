09672910980b2b48267051d6b4e9a232989e663e
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-05-25 19:40:05 -0400

    Added a new banner!

---
+++ profile.js
@@ -0,0 +1,17 @@
+
+
+
+var bearEvans = (function() {
+    var isHireable = true;
+    var skills = ["HTML", "CSS", "Javascript"];
+    var contact = {
+        Email : "bear-evans@outlook.com",
+        gitHub : "https://github.com/bear-evans/"
+    };
+});
+
+
+
+
+
+
